3e10de0f633340cd14962947058dbc0d5fe301fd
Rigel <rigel.rahateen@student.president.ac.id>
2025-03-27 19:19:39 +0700

    feat: test images

--- src/features/post-generator/data/characters.ts
+++ src/features/post-generator/data/characters.ts
@@ -18,9 +18,9 @@ export const Characters: Record<string, Record<string, CharacterData>> = {
         'Acheron': {
             name: 'Acheron',
             images: [
-                '/assets/post-generator/characters/honkai/kiana/kiana_1.webp',
-                '/assets/post-generator/characters/honkai/kiana/kiana_2.webp',
-                '/assets/post-generator/characters/honkai/kiana/kiana_3.webp',
+                '/assets/post-generator/characters/hsr/kiana/kiana_1.webp',
+                '/assets/post-generator/characters/hsr/kiana/kiana_2.webp',
+                '/assets/post-generator/characters/hsr/kiana/kiana_3.webp',
             ],
         },
         
@@ -29,9 +29,9 @@ export const Characters: Record<string, Record<string, CharacterData>> = {
         'Miyabi': {
             name: 'Miyabi',
             images: [
-                '/assets/post-generator/characters/star_rail/march_7th/march_7th_1.webp',
-                '/assets/post-generator/characters/star_rail/march_7th/march_7th_2.webp',
-                '/assets/post-generator/characters/star_rail/march_7th/march_7th_3.webp',
+                '/assets/post-generator/characters/zzz/march_7th/march_7th_1.webp',
+                '/assets/post-generator/characters/zzz/march_7th/march_7th_2.webp',
+                '/assets/post-generator/characters/zzz/march_7th/march_7th_3.webp',
             ],
         },
     },
