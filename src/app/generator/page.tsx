803e55326faaf8323aa383a181894480011c4c17
Rigel <rigel.rahateen@student.president.ac.id>
2025-03-29 21:40:03 +0700

    feat: added shadcn

--- src/app/generator/page.tsx
+++ src/app/generator/page.tsx
@@ -2,8 +2,11 @@ import PostForm from "@/features/post-generator/components/PostForm"
 
 export default function PostGeneratorPage () {
     return (
-        <div className="flex items-center justify-center mt-4">
-            <PostForm />
-        </div>
+        <main className="min-h-screens py-8">
+            <div className="container mx-auto px-4">
+                <h1 className="text-2xl font-bold text-gray-800 mb-8 text-center">Post Generator</h1>
+                <PostForm />
+            </div>
+        </main>
     )
 }
