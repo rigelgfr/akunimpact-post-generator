15f89235d8e49246445c3de8a6de99052f4c3001
Rigel <rigel.rahateen@student.president.ac.id>
2025-04-06 19:06:05 +0700

    feat: simple page nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,26 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen space-y-2">
       <Image
         src="/logo/logo.png"
         alt="Vercel Logo"
-        width={72}
-        height={16}
+        width={100}
+        height={100}
       />
+
+      <div className="flex items-center justify-center gap-2">
+        <Button>
+          <Link href="/generator">Create a post</Link>
+        </Button>
+
+        <Button>
+          <Link href="/detector">Test the model</Link>
+        </Button>
+      </div>
     </div>
   );
 }
